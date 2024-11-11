2f55a5706434cef18ba1c9d7962b1bd17eb11792
canHamzaCode <abdulmuizzayo6@gmail.com>
2024-11-11 13:20:14 +0100

    fix

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -11,7 +11,7 @@ const ProtectedRoute = ({ children }: IProps) => {
         return <Navigate to='/' />;
     }
 
-    if (user.role !== 'admin') {
+    if (user && user.role !== 'admin') {
         return <Navigate to='/' />;
     }
     return children;
